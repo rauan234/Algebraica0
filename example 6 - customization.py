a42b412b43cfae099d71436b928866fb257e7668
rauan234 <39853697+rauan234@users.noreply.github.com>
2020-04-29 14:09:22 +0600

    Update example 6 - customization.py

--- example 6 - customization.py
+++ example 6 - customization.py
@@ -1,5 +1,17 @@
 from Algebraica import cp
 import Algebraica
+# Algebraica library is tunable.
+# in this example, we will explore this possibility.
+
+
+# Algebraica automatically simplifies certain expressions
+print('ln(e) =', cp( (1, ('ln', cp('e'))) ))
+
+# if you don`t want this, turn AutoSimpify off
+Algebraica.AutoSimplify = False
+
+print('ln(e) =', cp( (1, ('ln', cp('e'))) ))
+print()
 
 
 # let`s say you want to create a variable that would tell
@@ -39,18 +51,19 @@ Algebraica.show_names()
 print()
 
 # there is no restrictions on functions` names
-Algebraica.log_function_name = 'logan'
+Algebraica.log_function_name = 'dog'
 Algebraica.ln_function_name = 'In'
 
-print(cp( ((1, 1), ('logan', cp(8), cp(2))) ).simplified())
+print(cp( (1, ('dog', cp(8), cp(2))) ).simplified())
 print()
 # everything still works like it did before
 
 Algebraica.set_names_to_default()
-Algebraica.sin_function_name = 'I can`t spare a buck, but that`s'
-Algebraica.pi_constant_name = 'a rock'
-print(cp( ((1, 1), ('I can`t spare a buck, but that`s', cp( ((1, 2), 'a rock'))))))
-print(cp( ((1, 1), ('I can`t spare a buck, but that`s', cp( ((1, 2), 'a rock'))))).simplified())
+
+Algebraica.sin_function_name = 'I_can`t_spare_a_buck_but_that`s'
+Algebraica.pi_constant_name = 'a_rock'
+print(cp( (1, ('I_can`t_spare_a_buck_but_that`s', cp( ((1, 2), 'a_rock'))))))
+print(cp( (1, ('I_can`t_spare_a_buck_but_that`s', cp( ((1, 2), 'a_rock'))))).simplified())
 print()
 Algebraica.set_names_to_default()
 # just one more example
@@ -58,20 +71,20 @@ Algebraica.set_names_to_default()
 
 # Also, you can adjust the indentation step
 Algebraica.TabulationStep = 6
-print(cp( ((1, 1), ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
+print(cp( (1, ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
 
 Algebraica.TabulationStep = 2
-print(cp( ((1, 1), ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
+print(cp( (1, ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
 
 Algebraica.TabulationStep = 4
-print(cp( ((1, 1), ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
+print(cp( (1, ('ln', cp(1, ((2, 3), ('sqrt', cp(3)))))) ))
 print()
 
 
 # if you, like me, prefer tau over pi, you can turn on the TauMode
 Algebraica.TauMode = True
 print(cp('pi').simplified())
-print(cp( ((1, 1), ('arcsin', cp(1)))).simplified())
+print(cp( (1, ('arcsin', cp(1)))).simplified())
 print()
 # if TauMode is on, pi is expressed as tau/2
 
@@ -80,11 +93,23 @@ Algebraica.TauMode = False
 print(cp('tau').simplified())
 print()
 
-# you can also turn on auto simplification so that all the expressions were clear
-# if AutoSimplification setting is active, all class Complex objects will be simplified before each print
-# be careful with this one, though, as it might give wrong answers due to possible bugs
-Algebraica.AutoSimplify = True
-print( cp( ((1, 1), ('ln', cp('e'))) ))
+
+# you can also change the precision with which class Numerical objects are displayed
+print(Algebraica.Numerical(1/3, 0))
+Algebraica.NumericalNumbersPrintLength = 7
+print(Algebraica.Numerical(1/3, 0))
+print()
+
+
+# if you want to see stuff in standard blackwhite, switch ColorfulPrint to False
+Algebraica.ColorfulPrint = False
+print( cp( (1, ('ln', cp(2))) ))
+print()
+
+
+# if you want, you can turn on the use of Euler`s formula for complex exponentials
+Algebraica.ExpandComplexExponentials = True
+print('exp(i) =', cp( (1, ('exp', cp(0, 1))) ).simplified())
 print()
 
 
