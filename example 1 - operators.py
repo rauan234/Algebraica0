c6a313770ea46a5b01f24c244f94070e021abd58
rauan234 <39853697+rauan234@users.noreply.github.com>
2020-04-29 14:06:45 +0600

    Update example 1 - operators.py

--- example 1 - operators.py
+++ example 1 - operators.py
@@ -1,6 +1,6 @@
 from Algebraica import cp
-# if creating complex numbers was all Algebraica was usable for, it would be pretty useless
-# luckily, Algebraica allows to perform a variety of operations with complex numbers
+# if creating complex numbers was all Algebraica was usable for, it would be pretty useless.
+# luckily, Algebraica allows to perform a variety of operations on complex numbers.
 
 
 A = cp(2, 3)
@@ -19,54 +19,7 @@ print()
 print('~A =', ~A)
 print()
 # this one returns a complex conjugate of A.
-# if A = a + b i, ~A = a + (-b) i
-
-print('A + B =', A + B)
-print()
-# that`s how two class Complex objects are added together
-
-print('A - B =', A - B)
-print()
-# that`s how two class Complex objects can be subtracted from each other
-
-print('A * B =', A * B)
-print()
-# that`s multiplication of two complex number
-
-print('A * 2 =', A * 2)
-print('A / 2 =', A / 2)
-print()
-# complex numbers can be multiplied and divided by integers
-
-print('A * 1.6 =', A * 1.6)
-print('A / 1.6 =', A / 1.6)
-print()
-# and floats
-
-print('A * pi =', A * 'pi')
-print()
-# you can even multiply Complex by string!
-# the result of this operation is A * cp('pi').
-
-print('A * stuff =', A * (['pi', [((1, 2), ('ln', cp(11))), ('sqrt', cp(2))], ('sqrt', cp(5))], [(1, 2), 'e']))
-print()
-# actually, you can write down any expression,
-# just as if you were creating a class Complex object.
-# in other words, A * (something) = A * cp(something)
-
-print('A / B =', A / B)
-print()
-# class Complex objects can be divided by other class Complex objects
-
-print('A ** +3 =', A ** 3)
-print('A ** 0 =', A ** 0)
-print('A ** -3 =', A ** -3)
-print()
-# they can be raised to integer powers
-
-print('A ** B =', A ** B)
-print()
-# they can also be raised to a complex power
+# if A = a + b i, then ~A = a + (-b) i.
 
 print('|A| =', abs(A))
 print()
@@ -76,15 +29,67 @@ print('arg(A) =', A.arg())
 print()
 # this is how you find the complex argument
 
-print('(A == B) =', A == B)
-print()
-# comparing two numbers
 
+print('A + 2 =', A + 2)
+print('A - 2 =', A - 2)
+print('A * 2 =', A * 2)
+print('A / 2 =', A / 2)
+print()
+# arithmerics can be done between Complex and ints
+
+print('A + 1.6 =', A + 1.6)
+print('A - 1.6 =', A - 1.6)
+print('A * 1.6 =', A * 1.6)
+print('A / 1.6 =', A / 1.6)
+print()
+# and floats
+
+print('A ** +3 =', A ** 3)
+print('A ** 0 =', A ** 0)
+print('A ** -3 =', A ** -3)
+print('A ** B =', A ** B)
+print()
+# that`s how class Complex objects can be raised to an integer power
+
+
+print('(A == B) =', A == B)
 print('(A != B) =', A != B)
 print()
-# checking if the two numbers are different
+# class Complex objects can be compared for equality.
+# there is no operators like (A > B), because it is not
+# clear what it should return when given a pair of complex numbers.
 
-C = A + B ** 7
+print('A + B =', A + B)
+print('A - B =', A - B)
+print('A * B =', A * B)
+print('A / B =', A / B)
+print()
+# here are the basic arithmetic operations involving two class Complex objects
+
+print(A ** B)
+print(B ** A)
+print()
+# class Complex objects can also be raised to a complex power
+
+i = cp(0, 1)  # the imaginary unit
+print(i ** i)
+print()
+# this, for example, is i ** i
+
+
+print('A * pi =', A * 'pi')
+print()
+# you can even multiply Complex by string!
+# the result of this operation is the same as A * cp('pi').
+
+print('A * stuff =', A * (['pi', [((1, 2), ('ln', cp(11))), ('sqrt', cp(2))], ('sqrt', cp(5))], [(1, 2), 'e']))
+print()
+# actually, you can write down any expression,
+# just as if you were creating a class Complex object.
+# in other words, A * (something) = A * cp(something).
+
+
+C = A + B ** 3
 D = (A / B) ** C
 C -= D
 E = cp(('exp', C))
@@ -92,9 +97,8 @@ print('C =', C)
 print('D =', D)
 print('E =', E)
 print()
-# doing operations with variables
-# as you might have noticed, the result sometimes is a real mess
-# so after getting the answer it`s useful to re-write it on paper and try to simplify
-# or try auto-simplification described in example 4
+# this is how you might want to use class Complex objects for your calculations.
+# notice, that the answer is automatically simplified.
+# view example 4 to learn more about simplification.
 
 input()
